cc798ae18168367444d4efc1078b49ab1be06467
Banzneri <pasi.hanhijarvi@gmail.com>
2022-01-28 09:46:55 +0200

    install eslint & refactor accordingly

--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -5,13 +5,13 @@ export const getUser = async (id) => {
     const user = await User.findByPk(id);
     return user;
   } catch (error) {
-    throw Error(`Error getting user by id: ${error.message}`)
+    throw Error(`Error getting user by id: ${error.message}`);
   }
-}
+};
 
 export const addUser = async (user) => {
   try {
-    const newUser = await User.create({...user});
+    const newUser = await User.create({ ...user });
     return newUser;
   } catch (error) {
     throw Error(`Error adding user: ${error.message}`);
