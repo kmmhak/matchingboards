31385310c130482a8c4ab8805ccfc573b1af3644
Banzneri <pasi.hanhijarvi@gmail.com>
2022-02-02 14:39:28 +0200

    add functions to delete users games

--- server/src/routes/game.router.js
+++ server/src/routes/game.router.js
@@ -7,5 +7,6 @@ gameRouter.get('/', controller.getAllGames);
 gameRouter.get('/:id', controller.getGameById);
 gameRouter.get('/search/name', controller.searchGames);
 gameRouter.post('/users', controller.addGameToUser);
+gameRouter.delete('/users', controller.deleteGameFromUser);
 
 export default gameRouter;
