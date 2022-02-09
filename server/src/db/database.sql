910234a3d7c92e9aa1cb5158e2157295ea2bbf77
Kristiina <kristiina.hakkala@gmail.com>
2022-02-09 13:44:08 +0000

    Update distance to user.model and database and validation to user.controller.js

--- server/src/db/database.sql
+++ server/src/db/database.sql
@@ -11,9 +11,39 @@ CREATE TABLE "users"(
   "zip_code" INT NOT NULL,
   "latitude" DECIMAL,
   "longitude" DECIMAL,
+  "distance" INT DEFAULT '10', 
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT Now(),
-  "updated_at" TIMESTAMPTZ DEFAULT Now()
+  "updated_at" TIMESTAMPTZ NOT NULL DEFAULT Now()
 );
+
+INSERT INTO "users"(
+  "id", 
+  "email", 
+  "username", 
+  "salt", 
+  "hash", 
+  "zip_code", 
+  "latitude", 
+  "longitude")
+VALUES (
+  '0', 
+  'anonymous@example.com', 
+  'anonymousUser', 
+  'salt', 
+  'hash', 
+  '99998', 
+  '0.0', 
+  '0.0');
+
+CREATE RULE protect_anonymous_delete AS 
+ON DELETE TO users
+WHERE old.id = '0'
+DO INSTEAD NOTHING;
+
+CREATE RULE protect_anonymous_update AS
+ON UPDATE TO users
+WHERE  old.id = 0
+DO INSTEAD NOTHING;
 
 create table "games" (
   "id" INT PRIMARY KEY,
@@ -34,6 +64,8 @@ create table "sessions" (
   "creator_id" INT NOT NULL,
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT Now(),
   "session_finished" BOOLEAN NOT NULL DEFAULT false,
+  "latitude" DECIMAL NOT NULL,
+  "longitude" DECIMAL NOT NULL,
   CONSTRAINT "game_id"
   FOREIGN KEY("game_id")
     REFERENCES "games"("id")
@@ -49,6 +81,7 @@ create table "sessions" (
 create table "users_games"(
   "user_id" INT NOT NULL,
   "game_id" INT NOT NULL,
+  "owned" BOOLEAN DEFAULT false,
   PRIMARY KEY ("user_id", "game_id"),
   FOREIGN KEY("user_id")
     REFERENCES "users"("id")
@@ -100,18 +133,18 @@ create table "users_sessions"(
 
 create table "ratings"(
   "value" INT NOT NULL,
-  "giver_id" INT NOT NULL,
-  "receiver_id" INT NOT NULL,
+  "giver_id" INT NOT NULL DEFAULT '0',
+  "receiver_id" INT NOT NULL DEFAULT '0',
   PRIMARY KEY("giver_id", "receiver_id"),
   CONSTRAINT "giver_id"
     FOREIGN KEY("giver_id")
     REFERENCES "users"("id")
-    ON DELETE CASCADE
+    ON DELETE SET DEFAULT
     ON UPDATE CASCADE,
   CONSTRAINT "receiver_id"
     FOREIGN KEY("receiver_id")
     REFERENCES "users"("id")
-    ON DELETE CASCADE
+    ON DELETE SET DEFAULT
     ON UPDATE CASCADE	
 );
 
@@ -154,18 +187,18 @@ create table "blacklist"(
 create table "messages"(
   "id" SERIAL PRIMARY KEY,
   "message" VARCHAR NOT NULL,
-  "sender_id" INT NOT NULL,
-  "receiver_id" INT NOT NULL,
+  "sender_id" INT NOT NULL DEFAULT '0',
+  "receiver_id" INT NOT NULL DEFAULT '0',
   "sent_at" TIMESTAMPTZ NOT NULL DEFAULT Now(),
   "has_read" BOOLEAN NOT NULL DEFAULT false,
   CONSTRAINT "sender_id"
   FOREIGN KEY("sender_id")
     REFERENCES "users"("id")
-    ON DELETE CASCADE
+    ON DELETE SET DEFAULT
     ON UPDATE CASCADE,
   CONSTRAINT "receiver_id"
   FOREIGN KEY("receiver_id")
     REFERENCES "users"("id")
-    ON DELETE CASCADE
+    ON DELETE SET DEFAULT
     ON UPDATE CASCADE
 );
--- server/src/models/user.game.model.js
+++ server/src/models/user.game.model.js
@@ -1,10 +1,16 @@
+import { DataTypes } from 'sequelize/dist';
 import sequelize from '../db/db.js';
 import Game from './game.model.js';
 import User from './user.model.js';
 
 const UserGame = sequelize.define(
   'UserGame',
-  {},
+  {
+    owned: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
   {
     tableName: 'users_games',
     timestamps: false,
