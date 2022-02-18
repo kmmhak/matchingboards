87ce7e2136f8c91ac0c5bf9835a11106d63112a2
Katja Puputti <katja.puputti@gmail.com>
2022-02-18 09:59:14 +0200

    Add search users by username bar

--- frontend/src/components/FindUser.jsx
+++ frontend/src/components/FindUser.jsx
@@ -1,6 +1,49 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: 'rgba(228, 212, 179, 0.6)',
+  '&:hover': {
+    backgroundColor: 'rgba(228, 212, 179, 0.824)',
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 export default function FindUser() {
   return (
@@ -12,12 +55,15 @@ export default function FindUser() {
       noValidate
       autoComplete="off"
     >
-      <TextField
-        id="outlined-basic"
-        label="Search by username"
-        variant="outlined"
-        style={{ backgroundColor: 'rgba(228, 212, 179, 0.824)' }}
-      />
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon sx={{ color: 'rgba(52, 45, 43, 0.959)' }} />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search by username"
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </Search>
     </Box>
   );
 }
