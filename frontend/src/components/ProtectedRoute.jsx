953fd8591a3e1216d0700cb90719f8dc99ada2bc
Banzneri <pasi.hanhijarvi@gmail.com>
2022-02-21 13:19:02 +0000

    add login persistence to frontend

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { useUser } from '../contexts/UserContext';
 
 function ProtectedRoute() {
-  const { loggedIn } = useUser();
+  const [isLoading, setIsLoading] = useState(true);
+  const { login } = useUser();
+  const navigate = useNavigate();
 
-  return loggedIn ? <Outlet /> : <Navigate to="/login" />;
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) navigate('/login');
+
+    axios
+      .get('http://localhost:3001/authenticate', {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((response) => {
+        const { user } = response.data;
+        login(user, token);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        navigate('/login');
+      });
+  }, []);
+
+  return !isLoading ? <Outlet /> : <p>Loading...</p>;
 }
 
 export default ProtectedRoute;
